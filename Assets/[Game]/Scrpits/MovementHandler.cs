c8265c6a1e318ba2e3d061ce7cd4dc03f2faab65
bucurum <berkeucurum7@gmail.com>
2022-12-24 19:17:49 +0300

    Added tilemapCollider2D to walls, zerofriction physics material to rigidbody and change walls` collider type to grid from sprite. Creating Pacman script and set up the movementHandler script`s functions

--- Assets/[Game]/Scrpits/MovementHandler.cs
+++ Assets/[Game]/Scrpits/MovementHandler.cs
@@ -10,12 +10,58 @@ public class MovementHandler : MonoBehaviour
     public Rigidbody2D rb2d {get; private set;}
     public Vector2 direction {get; private set;}
     public Vector2 nextDirection {get; private set;}
-    
+    public Vector2 startPosition {get; private set;}
+
     void Awake()
     {
         this.rb2d = this.GetComponent<Rigidbody2D>();
+        this.startPosition = this.transform.position;
     }
 
+    void Start()
+    {
+        ResetState();
+    }
 
+    public void ResetState()
+    {
+        this.speedMultiplier = 1f;
+        this.direction = this.initalDirection;
+        this.nextDirection = Vector2.zero;
+        this.transform.position = this.startPosition;
+        this.rb2d.isKinematic = false;
+        this.enabled = true;
+    }
 
+    void FixedUpdate() //Physics is needed to be fixedupdate
+    {
+        Vector2 position = this.rb2d.position;
+        Vector2 translation = this.direction * this.speed * this.speedMultiplier * Time.fixedDeltaTime;
+        this.rb2d.MovePosition(position + translation);
+    }
+    void Update()
+    {
+        if (this.nextDirection != Vector2.zero)
+        {
+            SetDirection(this.nextDirection);
+        }
+    }
+    public void SetDirection(Vector2 direction, bool forced = false)
+    {
+        if (forced || !Occupied(direction))
+        {
+            this.direction = direction;
+            this.nextDirection = Vector2.zero;
+        }
+        else
+        {
+            this.nextDirection = direction;
+        }
+    }
+
+    public bool Occupied(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * .75f, 0f, direction, 1.5f, this.obstacleLayerMask);
+        return hit.collider != null;
+    }
 }
--- Assets/[Game]/Scrpits/Pacman.cs
+++ Assets/[Game]/Scrpits/Pacman.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MovementHandler))]
 public class Pacman : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public MovementHandler movement {get; private set;} 
+    
+    void Awake()
     {
-        
+        this.movement = GetComponent<MovementHandler>();
     }
-
-    // Update is called once per frame
+  
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.movement.SetDirection(Vector2.up);
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.movement.SetDirection(Vector2.down);
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.movement.SetDirection(Vector2.right);
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.movement.SetDirection(Vector2.left);
+        }
+        float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
+        this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
     }
+
+
+
 }
